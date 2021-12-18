90a72b250f11f28cfd97a8fd00db963d79939671
Andrew Brittenham <sirandrewking.git@gmail.com>
2021-12-17 20:27:39 -0600

    added the new game functionality. changed the start button functionality as well.

--- JS/app.js
+++ JS/app.js
@@ -1,4 +1,5 @@
 const starts = document.querySelectorAll(".start");
+const newGs = document.querySelectorAll(".newG");
 
 // these update where they get called from (anywhere "janken(usrInput)" is)
 let usrScore = 0;
@@ -6,21 +7,21 @@ let cpuScore = 0;
 let tieScore = 0;
 let totalGames = 0;
 
+
+
 // clicking the start button on the site starts the game
 // the game is not operational until the start button is clicked
 starts.forEach((start) => {
     start.addEventListener("click", function() {
 
-        // next 5 lines remove the start button once it is clicked.
-        let startBtn = document.getElementById("start");
-
-        while(startBtn.firstChild) {
-            startBtn.removeChild(startBtn.firstChild)
-        };
+        document.getElementById("start").style.display = "none";
         // unhides the game div (none > block)
-        document.getElementById('game').style.display = "block";
+        document.getElementById("game").style.display = "block";
         // everything after this line only works when the start button is clicked.
         const options = document.querySelectorAll(".option");
+
+        // placed here to load immediatly after game is started
+        newGame();
 
         options.forEach((option) => {
             option.addEventListener("click", function(){
@@ -29,15 +30,16 @@ starts.forEach((start) => {
                 let result = document.getElementById("result");
 
                 if (result.hasChildNodes()) {
-                    result.removeChild(result.childNodes[0]);
+                        result.removeChild(result.childNodes[0]);
                 };
-                janken(usrInput)
-                updateScore()
+
+                janken(usrInput);
+                updateScore();
+
             });
         });
     });
 });
-
 
 
 function janken(usrInput){
@@ -66,8 +68,23 @@ function janken(usrInput){
 };
 
 
-function updateScore() {
+function updateScore(){
       document.getElementById("usrScore").textContent = usrScore;
       document.getElementById("cpuScore").textContent = cpuScore;
       document.getElementById("tieScore").textContent = tieScore;
-    }
+    };
+
+
+function newGame(){
+
+    newGs.forEach((newG) => {
+        newG.addEventListener("click", function(){
+
+            //hides the game div
+            document.getElementById("game").style.display = "none";
+            //unhides the start div
+            document.getElementById("start").style.display = "block";
+            
+        });
+    });
+};
